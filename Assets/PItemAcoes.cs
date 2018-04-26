409567450e9ff3f0fdb3ddf31a8495e019e7b9f4
Guilherme rodrigues <jogos.coisas@gmail.com>
2018-04-25 21:16:49 -0300

    First commit

---
+++ Assets/PItemAcoes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PItemAcoes : MonoBehaviour {
+	private GameObject Next;
+	private GameObject Player;
+	private Rigidbody2D rb2d;
+	private BoxCollider2D bc2d;
+
+	private float size;
+	// Use this for initialization
+	void Start () {
+		Next = null;
+		rb2d = GetComponent<Rigidbody2D> ();
+		bc2d = GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Follow ();
+	}
+
+	public void SetFollow(GameObject objPlayer, GameObject obj){
+		Player = objPlayer;
+		Next = obj;
+		rb2d.isKinematic = true;
+		bc2d.isTrigger = true;
+		size = (Next == Player) ? 0.3f : 0;
+	}
+	void Follow(){
+		if (Next != null) {
+			transform.position = Vector2.Lerp(transform.position, new Vector2(Next.transform.position.x, Next.transform.position.y -size), Time.deltaTime * 2f);
+		}
+	}
+}
---
+++ Assets/player/PlayerMove.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	//defaults
+	private float _SpeedDefault;
+	private float _ForceJumpDefault;
+
+	protected List<GameObject> PItens;
+	protected Rigidbody2D rb2d;
+	[SerializeField]
+	private float _Speed, _ForceJump;
+	[SerializeField]
+	private int _NumberJump;
+
+	void Awake(){
+		_SpeedDefault = 4;
+		_ForceJumpDefault = 1700;
+	}
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.freezeRotation = true;
+		_NumberJump = 2;
+		_Speed = _SpeedDefault;
+		_ForceJump = _ForceJumpDefault;
+		PItens = new List<GameObject> ();
+	}
+
+	void FixedUpdate () {
+		
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && _NumberJump > 0) {
+			Jump ();
+			_NumberJump--;
+		}
+		if (Input.GetKey(KeyCode.A))
+			MoveLeft ();
+		if (Input.GetKey(KeyCode.D))
+			MoveRigth ();
+	}
+
+	void MoveRigth(){
+		transform.position += ((Vector3.right * _Speed) * Time.deltaTime);
+	}
+
+	void MoveLeft(){
+		transform.position += ((Vector3.left * _Speed) * Time.deltaTime);
+	}
+
+	void Jump(){
+		rb2d.AddForce (new Vector2(0,10 * _ForceJump) * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Plane" && coll.contacts [0].normal.y == 1) {
+			_NumberJump = 2;
+			_Speed = _SpeedDefault;
+			_ForceJump = _ForceJumpDefault;
+		} else if (coll.gameObject.tag == "PItem") {
+			if (PItens.Count == 0) {
+				coll.gameObject.GetComponent<PItemAcoes> ().SetFollow(gameObject, gameObject);
+			} else {
+				coll.gameObject.GetComponent<PItemAcoes> ().SetFollow (gameObject, PItens[PItens.Count-1]);
+			}
+			PItens.Add (coll.gameObject);
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Plane") {
+			_Speed /= 1.1f;
+		}
+	}
+}
